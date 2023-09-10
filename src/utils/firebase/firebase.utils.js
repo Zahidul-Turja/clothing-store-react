7b508bae6d51bbdf49374f8a2e1ff8764f8599a0
Zahidul-Turja <zahidulturja@gmail.com>
2023-09-10 15:02:49 +0600

    added out sign in component as well as firebase utils and brought in firebase users

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -32,8 +32,22 @@ export const db = getFirestore();
 
 export const createUserDocumentFromAuth = async (userAuth) => {
   const userDocRef = doc(db, "users", userAuth.uid);
-
   const userSnapShot = await getDoc(userDocRef);
 
-  console.log(userSnapShot.exists());
+  if (!userSnapShot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log("error creating the user", error.message);
+    }
+  }
+
+  return userDocRef;
 };

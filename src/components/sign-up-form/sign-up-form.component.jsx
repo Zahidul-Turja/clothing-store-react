3774e6f3f1f7ece14a6d7fce73b1bc1e9811d78f
Zahidul-Turja <zahidulturja@gmail.com>
2023-09-24 12:52:52 +0600

    scss converted to styled-components

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -8,7 +8,7 @@ import {
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
 
-import "./sign-up-form.styles.scss";
+import { SignUpContainer } from "./sign-up-form.styles";
 
 const defaultFormFields = {
   displayName: "",
@@ -28,10 +28,8 @@ const SignUpForm = () => {
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-    if (password != confirmPassword) {
-      alert("Password do not match!");
-      console.log(password);
-      console.log(confirmPassword);
+    if (password !== confirmPassword) {
+      alert("passwords do not match");
       return;
     }
 
@@ -40,16 +38,15 @@ const SignUpForm = () => {
         email,
         password
       );
-      await createUserDocumentFromAuth(user, {
-        displayName,
-      });
 
+      await createUserDocumentFromAuth(user, { displayName });
       resetFormFields();
     } catch (error) {
-      if (error.code == "auth/email-already-in-use") {
-        alert("Can't create user, email already in use!");
+      if (error.code === "auth/email-already-in-use") {
+        alert("Cannot create user, email already in use");
+      } else {
+        console.log("user creation encountered an error", error);
       }
-      console.log("User creating encountered an error", error);
     }
   };
 
@@ -60,7 +57,7 @@ const SignUpForm = () => {
   };
 
   return (
-    <div className="sign-up-container">
+    <SignUpContainer>
       <h2>Don't have an account?</h2>
       <span>Sign up with your email and password</span>
       <form onSubmit={handleSubmit}>
@@ -99,10 +96,9 @@ const SignUpForm = () => {
           name="confirmPassword"
           value={confirmPassword}
         />
-
         <Button type="submit">Sign Up</Button>
       </form>
-    </div>
+    </SignUpContainer>
   );
 };
 
